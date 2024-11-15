19c8d65006cfe80a2f2b281f58b2e2703feb57d1
lsuliverl <sh000803@naver.com>
2024-11-15 15:49:32 +0900

    todo 추가하기 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
 function App() {
-    return <div></div>;
+    const [toDo, setToDo] = useState("");
+    const [toDos, setToDos] = useState([]);
+    const onChange = (event) => setToDo(event.target.value);
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (toDo === "") {
+            return;
+        }
+        setToDos((currentArray) => [toDo, ...currentArray]);
+        setToDo("");
+    };
+    return (
+        <div>
+            <h1>나의 할 일들 ({toDos.length})</h1>
+            <form onSubmit={onSubmit}>
+                <input onChange={onChange} value={toDo} type="text" placeholder="해야 할일..." />
+                <button>추가</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
